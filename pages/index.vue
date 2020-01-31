a4aa8dde3b90c2e1c9f2dfc41e4ad58a3b07e2eb
Eliezer Eliezer <eliezer@Eliezers-MacBook-Air.local>
2020-01-31 19:49:59 +0200

    creates the skeleton2

--- pages/index.vue
+++ pages/index.vue
@@ -1,41 +1,20 @@
 <template>
   <div class="container">
     <div>
-      <logo />
-      <h1 class="title">
-        PelePack
-      </h1>
-      <h2 class="subtitle">
-        PelePack keeps your food looking great.
-      </h2>
+      <h1 class="title">PelePack</h1>
+      <h2 class="subtitle">PelePack keeps your food looking great.</h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
+        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
-  components: {
-    Logo
-  }
-}
+  components: {}
+};
 </script>
 
 <style>
@@ -49,8 +28,8 @@ export default {
 }
 
 .title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
